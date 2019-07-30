8e3eace6ee5ece437f3d73b6b1608b529d2f10c1
Taylan Doğan <taylan.dogan@masomo.com>
2019-07-30 23:18:23 +0300

    fix(parser): remove unnecessary calls in main
    Signed-off-by: Taylan Doğan <taylan.dogan@masomo.com>

--- cmd/mangapplizer/main.go
+++ cmd/mangapplizer/main.go
@@ -1,74 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"image"
-	"image/png"
-	"io"
 	"log"
 	mangarock "mangapplizer-backend/pkg/parser/mangarock"
-	"net/http"
-	"os"
-	"strings"
+	"strconv"
 )
 
 const (
 	anilistURL = "https://graphql.anilist.co/"
 )
-
-func mriTest(mr *mangarock.Client) {
-	// m, e := mr.Manga("mrs-serie-100177863")
-	m, e := mr.Chapter("mrs-serie-100177863", "mrs-chapter-100177864")
-	if e != nil {
-		log.Fatal(e)
-		return
-	}
-	fmt.Println(m.Pages[0])
-
-	saveMRI(m.Pages[0])
-
-	// testing mri to png
-	r, err := os.Open("/tmp/tmp.mri")
-	if err != nil {
-		log.Fatal("Cannot open the file")
-	}
-	img, _, err := image.Decode(r)
-	if err != nil {
-		log.Fatal("Cannot decode the MRI file")
-	}
-
-	out := strings.TrimSuffix("/tmp/tmp.mri", ".mri") + ".png"
-
-	w, err := os.Create(out)
-	if err != nil {
-		log.Fatal("cannot create the outpu")
-	}
-	if err := png.Encode(w, img); err != nil {
-		log.Fatal("Could not encode png")
-	}
-}
-
-func saveMRI(url string) {
-	response, e := http.Get(url)
-	if e != nil {
-		log.Fatal(e)
-	}
-	defer response.Body.Close()
-
-	//open a file for writing
-	file, err := os.Create("/tmp/tmp.mri")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	// Use io.Copy to just dump the response body to the file. This supports huge files
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Success!")
-}
 
 func main() {
 	// q := anilist.New(anilistURL)
@@ -79,6 +19,25 @@ func main() {
 	options := make(map[string]string)
 	options["country"] = "United States"
 	mr := mangarock.New(mangarock.WithOptions(options))
-	mriTest(mr)
+	// TODO: fetch serie id and chapter id dynamically.
+	chapter, e := mr.Chapter("mrs-serie-243382", "mrs-chapter-200100701")
+	if e != nil {
+		log.Fatal(e)
+		return
+	}
+
+	// TODO: put decent structure for path
+	// basePath + mangaName + language(like en/jp) + chapterOrder/
+	path := "/tmp/" + strconv.Itoa(chapter.Order) + "/"
+
+	err := mangarock.SaveChapter(&chapter, path)
+	if err != nil {
+		panic(err)
+	}
+
+	err = mangarock.ConvertMRItoPNG(path)
+	if err != nil {
+		panic(err)
+	}
 
 }
