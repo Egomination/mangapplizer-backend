cd7c5239f20d6281ec9fb37cd3d428d99931ff82
Taylan Dogan <doganntaylan@gmail.com>
2019-10-17 00:54:36 +0300

    feat(database): add ability to insert into database
    Still incomplete but does the basic level of insertions.
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- pkg/database/postgre.go
+++ pkg/database/postgre.go
@@ -1,9 +1,10 @@
-package database
+package main
 
 import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	// we don't need to use it but its necessary to connect db
@@ -14,23 +15,42 @@ import (
 
 var db *gorm.DB //database
 
-type manga struct {
-	gorm.Model
-	MangaID     uuid.UUID `gorm:"type:uuid;primary_key:true;"`
-	CoverImage  string    `gorm:"type:text;"`
-	BannerImage string    `gorm:"type:text;"`
-	StartDate   string    `gorm:"type:text;"`
-	EndDate     string    `gorm:"type:text;"`
-	Status      string    `gorm:"type:text;"`
-	Staff       []staff   `gorm:"many2many:Series;association_foreignkey:AuthorID;foreignkey:MangaID"`
+type Base struct {
+	ID        uuid.UUID `gorm:"type:uuid;primary_key;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
 }
 
-type staff struct {
-	gorm.Model
-	AuthorID uuid.UUID `gorm:"type:uuid;primary_key:true;"`
-	Role     string    `gorm:"type:text;"`
-	Name     string    `gorm:"type:text;"`
-	Image    string    `gorm:"type:text;"`
+// Manga is the main media resource
+type Manga struct {
+	Base
+	CoverImage    string
+	BannerImage   string
+	StartDate     string
+	EndDate       string
+	Status        string
+	Staff         []Staff `gorm:"many2many:series;"`
+	Title         string
+	Relationships []Relations
+}
+
+// Staff is the staff of the Media. This could be either author or artist
+type Staff struct {
+	Base
+	Role  string
+	Name  string
+	Image string
+}
+
+// Relations are the media that manga has relationship with. Like anime adaptation and such.
+type Relations struct {
+	Base
+	BannerImage      string
+	Title            string
+	Type             string
+	Status           string
+	RelationshipType string
 }
 
 // Initialize ..
@@ -59,10 +79,107 @@ func Initialize() {
 
 	db = conn
 
-	db.Debug().AutoMigrate(&manga{}, &staff{}) // tables
+	// db.LogMode(true)
+	// db.Debug().AutoMigrate(&User{}, &Profile{}) // tables
 }
 
 // GetDB returns to the database object
 func GetDB() *gorm.DB {
 	return db
+}
+
+// AddStaff adds new staff
+func AddStaff(db *gorm.DB, manga *Manga, staff *Staff) error {
+	res := db.Model(manga).Association("Staff").Append(staff)
+	return res.Error
+}
+
+func CreateManga(db *gorm.DB, manga *Manga) (uuid.UUID, error) {
+	res := db.Create(manga)
+	if res.Error != nil {
+		uid, _ := uuid.FromString("")
+		return uid, res.Error
+	}
+	return manga.ID, nil
+}
+
+func CreateStaffIfNotExists(db *gorm.DB, staffName string) (*Staff, error) {
+	var stf Staff
+	res := db.FirstOrCreate(&stf, Staff{Name: staffName})
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return &stf, nil
+}
+
+func NewManga(db *gorm.DB, manga *Manga) (*Manga, error) {
+	m := &Manga{
+		BannerImage: manga.BannerImage,
+		CoverImage:  manga.CoverImage,
+		Title:       manga.Title,
+		Status:      manga.Status,
+		EndDate:     manga.EndDate,
+		StartDate:   manga.StartDate,
+	}
+	tx := db.Begin()
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	_, err := CreateManga(tx, m)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, stf := range manga.Staff {
+		s, err := CreateStaffIfNotExists(tx, stf.Name)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+		err = AddStaff(tx, m, s)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+
+	res := tx.Commit()
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return &Manga{Title: manga.Title}, nil
+}
+
+func main() {
+	Initialize()
+	manga := Manga{
+		CoverImage:  "https://someothershit",
+		BannerImage: "https://someothershitothershit",
+		Title:       "Oh! Shit",
+		Status:      "On going",
+		EndDate:     "-",
+		StartDate:   "20/01/2015",
+		Staff: []Staff{
+			{
+				Name:  "Ali",
+				Role:  "Author",
+				Image: "https://someshit.com",
+			},
+		},
+		Relationships: []Relations{
+			{
+				RelationshipType: "Adaptation",
+				Type:             "Anime",
+				BannerImage:      "https://uggh",
+				Status:           "Finished",
+				Title:            "Some shtty",
+			},
+		},
+	}
+	_, e := NewManga(db, &manga)
+	if e != nil {
+		log.Fatalln(e)
+	}
+
 }
