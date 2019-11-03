2d9b2c626e398a6ff0aee571bdebbe128c89e82f
Taylan Dogan <doganntaylan@gmail.com>
2019-11-03 19:39:46 +0300

    feat: add actix and actix-web
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate actix_web;
 extern crate mangapplizer_backend;
 
 pub mod db_connection;
@@ -11,8 +12,20 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
+
+use actix_web::{
+    web,
+    App,
+    HttpRequest,
+    HttpResponse,
+    HttpServer,
+};
 // use mangapplizer_backend::test_print_data;
 // use mangapplizer_backend::test_get_data_from_db;
+
+fn index(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().json("Hello World!")
+}
 
 fn main() {
     // let _a = test_print_data();
@@ -20,6 +33,13 @@ fn main() {
     let my_uuid =
         uuid::Uuid::parse_str("10931acb-dd12-4f13-ac02-82a3372a7acf").unwrap();
     println!("{}", my_uuid.to_urn());
+
+    let _ = HttpServer::new(|| {
+        App::new().service(web::resource("/").route(web::get().to_async(index)))
+    })
+    .bind("0.0.0.0:9092")
+    .unwrap()
+    .run();
 
     // test_get_data_from_db(my_uuid);
 }
