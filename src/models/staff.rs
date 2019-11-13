4abc29a4447524a5614ab5f4b5dccd303eb38be9
Taylan Dogan <doganntaylan@gmail.com>
2019-11-13 21:44:50 +0300

    style: rustfmt
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- src/models/staff.rs
+++ src/models/staff.rs
@@ -3,23 +3,25 @@ use diesel::PgConnection;
 
 #[derive(Queryable, Identifiable, Associations, Debug)]
 pub struct Staff {
-    pub id:         uuid::Uuid,
-    pub created_at: Option<std::time::SystemTime>,
-    pub deleted_at: Option<std::time::SystemTime>,
-    pub updated_at: Option<std::time::SystemTime>,
-    pub anilist_id: i64,
-    pub staff_role: String,
-    pub staff_name: String,
-    pub image:      String,
+    pub id:          uuid::Uuid,
+    pub created_at:  Option<std::time::SystemTime>,
+    pub deleted_at:  Option<std::time::SystemTime>,
+    pub updated_at:  Option<std::time::SystemTime>,
+    pub anilist_id:  i64,
+    pub staff_role:  String,
+    pub staff_name:  String,
+    pub image:       String,
+    pub description: String,
 }
 
 #[derive(Insertable, Debug)]
 #[table_name = "staffs"]
 pub struct NewStaff {
-    pub anilist_id: i64,
-    pub staff_role: String,
-    pub staff_name: String,
-    pub image:      String,
+    pub anilist_id:  i64,
+    pub staff_role:  String,
+    pub staff_name:  String,
+    pub image:       String,
+    pub description: String,
 }
 
 impl NewStaff {
