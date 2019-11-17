37efd8aaaa92f032845a8e8e1d416722f72b086a
Taylan Dogan <doganntaylan@gmail.com>
2019-11-18 01:57:59 +0300

    feat: add find(full) function
    Well, I don't know why I call this function full but there's a story I
    must tell regarding this function.
    
    This is the most optimal result that I come up with. The reason is, my
    previous attempts created monstrous outputs such as
    ```rust
    [
    	{
    		MangaName: "something",
    		... rest of the manga info
    	},
    	{
    		StaffName: "something",
    		... rest of the staff info,
    	},
    	{
    		StaffName: "Another staff",
    	},
    ]
    ```
    and so on. As you can see, this format is horrendous. Now with the
    `Resp` struct it looks more pretty and it become parsable as
    well. Until I find a better way to handle this, this implementation
    will be the solution.
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- src/models/manga.rs
+++ src/models/manga.rs
@@ -86,3 +86,52 @@ impl<'a> NewManga<'a> {
             .get_result(connection)
     }
 }
+
+// TODO: put resp into separate file or into more proper file
+#[derive(Serialize)]
+pub struct Resp {
+    pub manga: Manga,
+    pub staff: Vec<crate::models::staff::Staff>,
+}
+
+impl Manga {
+    pub fn full(
+        mid: String,
+        connection: &PgConnection,
+    ) -> Result<Resp, diesel::result::Error> {
+        use crate::diesel::ExpressionMethods;
+        use crate::diesel::QueryDsl;
+        use crate::diesel::RunQueryDsl;
+        use crate::models::{
+            series::Series,
+            staff::Staff,
+        };
+        use crate::schema::{
+            series,
+            staffs,
+        };
+
+        let uid = uuid::Uuid::parse_str(&mid).unwrap();
+        let manga: Manga = mangas::table
+            .filter(mangas::id.eq(uid))
+            .get_result(connection)?;
+        let series: Result<Vec<Series>, diesel::result::Error> = series::table
+            .filter(series::manga_id.eq(uid))
+            .get_results(connection);
+        let staff_vec: Vec<Staff> = series
+            .into_iter()
+            .flatten()
+            .flat_map(|k| {
+                staffs::table
+                    .filter(staffs::id.eq(k.staff_id))
+                    .get_results(connection)
+                    .unwrap()
+            })
+            .collect::<Vec<Staff>>();
+
+        Ok(Resp {
+            manga: manga,
+            staff: staff_vec,
+        })
+    }
+}
