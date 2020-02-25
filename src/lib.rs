f88ea4a68e49be5c73336c55e32f0d2aff8d4908
Taylan Dogan <doganntaylan@gmail.com>
2020-02-25 22:58:25 +0300

    feat: disable lib.rs
    Since this is not a library implementation, I believe there's no need
    for lib.rs
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-#[macro_use]
-extern crate diesel;
-extern crate dotenv;
-extern crate serde;
-extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
-extern crate actix;
-extern crate actix_web;
+// #[macro_use]
+// extern crate diesel;
+// extern crate dotenv;
+// extern crate serde;
+// extern crate serde_json;
+// #[macro_use]
+// extern crate serde_derive;
+// extern crate actix;
+// extern crate actix_web;
 
-pub mod db_connection;
-pub mod errors;
-pub mod handlers;
-pub mod models;
-pub mod schema;
+// pub mod db_connection;
+// pub mod errors;
+// pub mod handlers;
+// pub mod models;
+// pub mod schema;
 // pub mod utils;
